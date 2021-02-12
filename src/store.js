11576f5befa2baa3ede371c415d4e88937ae7a21
Chris Sullivan <chrissullivan.dev@gmail.com>
2021-02-12 00:39:13 -0800

    Replace noop with Default reducer

--- src/store.js
+++ src/store.js
@@ -30,7 +30,7 @@ export const createStore = (options = {}) => {
   // This is due to peristance layer trying to create state - spoof reducers until they 
   // are dynamically created
   const spoofs = Object.assign({}, opts.state || {});
-  const spoofedReducers = Object.keys(opts.state || {}).reduce((a, k) => (a[k] = noop, a), {});
+  const spoofedReducers = Object.keys(opts.state || {}).reduce((a, k) => (a[k] = DEFAULT_REDUCER, a), {});
   const sagas = {};
   const reducers = { ...spoofedReducers, __core: DEFAULT_REDUCER };
   const combined = (s, a) => combineReducers(reducers)(s, a);
